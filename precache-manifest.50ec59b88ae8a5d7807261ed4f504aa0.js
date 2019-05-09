37431cb8a40962135845a3b63d40c91298bd435d
BambusMalpa <you@example.combambusmalpa@gmail.com>
2019-05-09 18:35:52 +0200

    Updates

---
+++ precache-manifest.50ec59b88ae8a5d7807261ed4f504aa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "49f477a17f5d4bffa22284ec79edbd79",
+    "url": "/React-Weather-App/static/media/cloud.49f477a1.png"
+  },
+  {
+    "revision": "bec730ca5bb383bc2c31",
+    "url": "/React-Weather-App/static/js/runtime~main.29983489.js"
+  },
+  {
+    "revision": "b0bb7529912ea4eafe9d",
+    "url": "/React-Weather-App/static/js/main.16f2284d.chunk.js"
+  },
+  {
+    "revision": "c940887e33274f4986c8",
+    "url": "/React-Weather-App/static/js/2.0d9d1180.chunk.js"
+  },
+  {
+    "revision": "b0bb7529912ea4eafe9d",
+    "url": "/React-Weather-App/static/css/main.304f167f.chunk.css"
+  },
+  {
+    "revision": "c940887e33274f4986c8",
+    "url": "/React-Weather-App/static/css/2.d2820cfa.chunk.css"
+  },
+  {
+    "revision": "7ddc50a179a66abcb53a7ed0e9130f10",
+    "url": "/React-Weather-App/index.html"
+  }
+];
