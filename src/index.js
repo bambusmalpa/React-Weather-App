b6dcd91a0cea57257fc27f31960cec5313bf0a41
bambusmalpa <bambusmalpa@gmail.com>
2019-09-23 16:03:37 +0200

    add bootstrap cdn to index.html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import "bootstrap/dist/css/bootstrap.css"
 import "./style.css"
 
 const initialData=JSON.parse(localStorage.getItem("initial"))
